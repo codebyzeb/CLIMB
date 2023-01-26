9fc546890a4fb9ec81e9bd990eaa005918ee923e
Zeb <zepsilon32@gmail.com>
2023-01-26 11:00:56 +0000

    Add evaluation script, UnMasked submodule and update setup and requirements

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 if [ ! -d "/env" ]; then
-	module load python/3.8
-	virtualenv -p python3.8 env
+	module load python/3.7
+	virtualenv -p python3.7 env
 	source env/bin/activate
 	pip install -r requirements.txt
 	pre-commit install
