cfc67d4ab1b7f4a9fd3cc3f5e8eba2d3cb7bace4
rdiehlmartinez <richarddiehl@gmail.com>
2023-01-27 19:25:52 +0000

    updating config struture and decomposing hierarchically

--- src/config.py
+++ src/config.py
@@ -2,63 +2,83 @@
 Followed from the BabyBERTa repo."""
 
 from dataclasses import dataclass
-from typing import Tuple, List
+from typing import Optional
 
 
 @dataclass
-class Paths:
-    # paths
-    data: str
-    big_data: str
-    dev_data: str
-    test_data: str
+class ExperimentParams:
+    seed: int
+
 
 @dataclass
 class DataParams:
-    # data
+    # name of the dataset on huggingface
+    dataset: str
+    subdomain: str
+
+    # sampling parameters
     sample_with_replacement: bool
-    consecutive_masking: bool
     training_order: str
-    num_sentences_per_input: int
+
+    # data processing parameters
+    tokenizer: str
     include_punctuation: bool
     allow_truncated_sentences: bool
-    num_mask_patterns: int
-    mask_pattern_size: int
-    probabilistic_masking: bool
-    mask_probability: float
-    leave_unmasked_prob_start: float
-    leave_unmasked_prob: float
-    random_token_prob: float
-    tokenizer: str
     add_prefix_space: bool
     max_input_length: int
-    corpora: List[str]
+    num_sentences_per_input: int
 
-@dataclass
-class TrainingParams:
-    # training
-    batch_size: int
-    lr: float
-    num_epochs: int
-    num_warmup_steps: int
-    weight_decay: float
-    seed: int
 
 @dataclass
 class ModelParams:
-    # model
-    load_from_checkpoint: str
+    # model parameters
+    model: str
+
+    load_from_checkpoint: bool
+    checkpoint_path: Optional[str]
+
     num_layers: int
-    hidden_size: int
     num_attention_heads: int
+    hidden_size: int
     intermediate_size: int
     initializer_range: float
     layer_norm_eps: float
 
 
 @dataclass
-class BabyBERTaConfig:
-    paths: Paths
-    data_params: DataParams
-    training_params: TrainingParams
-    model_params: ModelParams
+class ObjectiveParams:
+    # training objective parameters
+
+    # NOTE: the objective can have arbitrary parameters, so
+    # we duck-type everything to be optional
+
+    # MLM-Related parameters
+    num_mask_patterns: Optional[int]
+    mask_pattern_size: Optional[int]
+    probabilistic_masking: Optional[bool]
+    mask_probability: Optional[float]
+    leave_unmasked_prob_start: Optional[float]
+    leave_unmasked_prob: Optional[float]
+    random_token_prob: Optional[float]
+    consecutive_masking: Optional[bool]
+
+
+@dataclass
+class TrainerParams:
+    batch_size: int
+
+    optimizer: str
+    scheduler: str
+    lr: float
+    num_epochs: int
+    num_warmup_steps: int
+    weight_decay: int
+
+
+@dataclass
+class BabyLMConfig:
+    experiment: ExperimentParams
+    data: DataParams
+    model: ModelParams
+    objective: ObjectiveParams
+    trainer: TrainerParams
