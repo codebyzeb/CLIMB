e06709e733d931a5e67099db3fe7cb2a841e6174
rdiehlmartinez <richarddiehl@gmail.com>
2023-03-10 15:17:14 +0000

    updating trainer class to use versioning system in huggingface

--- src/trainer.py
+++ src/trainer.py
@@ -1,21 +1,112 @@
-from transformers import Trainer, TrainingArguments
-
-from torch.utils.data import DataLoader, RandomSampler
+import os
+import shutil
+from pathlib import Path
 
 import datasets
-import torch
+from huggingface_hub import Repository, create_repo
+from torch.utils.data import DataLoader
+from transformers import Trainer
+from transformers.trainer_utils import HubStrategy
+from transformers.utils import get_full_repo_name
 
 
-class CustomTrainer(Trainer): 
+class CustomTrainer(Trainer):
+    def __init__(self, experiment_group: str, experiment_name: str, **kwargs):
+        """
+        We need to override the __init__ method to add the experiment group and experiment name.
+        We use the group name and experiment name for version controlling/identifying the current
+        run in, for example, huggingface, wandb ...
 
-    def get_train_dataloader(self): 
+        Args:
+            experiment_group (str): Name of the group that the current experiment belongs to
+            experiment_name (str): Name of the experiment - needs to be set at runtime
+        """
+        self.experiment_group = experiment_group
+        self.experiment_name = experiment_name
+        super().__init__(**kwargs)
+
+    def init_git_repo(self, at_init: bool = False):
+        """
+        Initializes a git repo in `self.args.hub_model_id`.
+        Args:
+            at_init (`bool`, *optional*, defaults to `False`):
+                Whether this function is called before any training or not. If `self.args.overwrite_output_dir` is
+                `True` and `at_init` is `True`, the path to the repo (which is `self.args.output_dir`) might be wiped
+                out.
+        """
+        if not self.is_world_process_zero():
+            return
+        if self.args.hub_model_id is None:
+            repo_name = Path(self.args.output_dir).absolute().name
+        else:
+            repo_name = self.args.hub_model_id
+        if "/" not in repo_name:
+            repo_name = get_full_repo_name(
+                repo_name, token=self.args.hub_token
+            )
+
+        # Make sure the repo exists.
+        create_repo(
+            repo_name,
+            token=self.args.hub_token,
+            private=self.args.hub_private_repo,
+            exist_ok=True,
+        )
+        try:
+            self.repo = Repository(
+                self.args.output_dir,
+                clone_from=repo_name,
+                token=self.args.hub_token,
+                revision=self.experiment_name,
+            )
+        except EnvironmentError:
+            if self.args.overwrite_output_dir and at_init:
+                # Try again after wiping output_dir
+                shutil.rmtree(self.args.output_dir)
+                self.repo = Repository(
+                    self.args.output_dir,
+                    clone_from=repo_name,
+                    token=self.args.hub_token,
+                    revision=self.experiment_name,
+                )
+            else:
+                raise
+
+        try:
+            # the branch name should have been created already by the `create_repo` call
+            self.repo.git_pull()
+        except OSError:
+            # if the repo is empty, the git_pull will fail
+            pass
+
+        # By default, ignore the checkpoint folders
+        if (
+            not os.path.exists(
+                os.path.join(self.args.output_dir, ".gitignore")
+            )
+            and self.args.hub_strategy != HubStrategy.ALL_CHECKPOINTS
+        ):
+            with open(
+                os.path.join(self.args.output_dir, ".gitignore"),
+                "w",
+                encoding="utf-8",
+            ) as writer:
+                writer.writelines(["checkpoint-*/"])
+
+        self.push_in_progress = None
+
+    def get_train_dataloader(self):
         train_dataset = self.train_dataset
         data_collator = self.data_collator
 
         if isinstance(train_dataset, datasets.Dataset):
-            train_dataset = self._remove_unused_columns(train_dataset, description="training")
+            train_dataset = self._remove_unused_columns(
+                train_dataset, description="training"
+            )
         else:
-            data_collator = self._get_collator_with_removed_columns(data_collator, description="training")
+            data_collator = self._get_collator_with_removed_columns(
+                data_collator, description="training"
+            )
 
         train_sampler = self._get_train_sampler()
 
@@ -27,5 +118,4 @@ class CustomTrainer(Trainer):
             drop_last=self.args.dataloader_drop_last,
             num_workers=self.args.dataloader_num_workers,
             pin_memory=self.args.dataloader_pin_memory,
-            #worker_init_fn=seed_worker,
         )
