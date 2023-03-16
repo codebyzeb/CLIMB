9e37de24239566df3e1cfd6496fccda6537cda12
rdiehlmartinez <richarddiehl@gmail.com>
2023-03-16 06:44:44 +0000

    updating scripts so that 1 - interaction sessions loads correct OS (Red HAT 8 distro) and 2- slurm script uses CL args

--- scripts/launch_interactive.sh
+++ scripts/launch_interactive.sh
@@ -1,4 +1,5 @@
 echo "Requesting ${1:-1} GPUs for interactive session"
 echo "Billing: ${2:-BUTTERY-SL2-GPU} -- If you are not a member of this project, please change this to your project"
 sintr --qos=INTR -A ${2:-BUTTERY-SL2-GPU} --gres=gpu:${1:-1} -t 1:0:0  -p ampere -N 1
-
+module purge 
+module load rhel8/default-amp
