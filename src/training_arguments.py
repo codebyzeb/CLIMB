98d9e07f78a42ca7d05d2b36e5f7e7c711844ff2
hmcgovern <56073830+hmcgovern@users.noreply.github.com>
2023-03-16 15:54:35 +0000

    vanilla curriculum learning based on a static data ordering according to a scoring_fn

--- src/training_arguments.py
+++
@@ -1,6 +0,0 @@
-from transformers import TrainingArguments
-
-class CustomTrainingArguments(TrainingArguments):
-    def __init__(self, sampling_strategy='None', **kwargs):
-        super().__init__(**kwargs)
-        self.sampling_strategy = sampling_strategy
