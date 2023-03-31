a4c136501ecfa571bf4443f46583f7ade50fb3e6
rdiehlmartinez <richarddiehl@gmail.com>
2023-03-31 21:33:52 +0100

    updating config format to decompose curriculum into objective and data driven

--- src/config.py
+++ src/config.py
@@ -1,8 +1,7 @@
-"""Defines the set of hyperparameters to be specified in the config file.
-Followed from the BabyBERTa repo."""
+"""Defines the set of hyperparameters to be specified in the config file."""
 
 from dataclasses import dataclass
-from typing import Any, Dict, List, Optional, Union
+from typing import Dict, List, Optional
 
 from omegaconf import MISSING
 
@@ -16,7 +15,7 @@ class ExperimentParams:
 
     # Name of the group that the current experiment belongs to
     # analogous to 'project' in wandb
-    group: str = "dev"
+    group: str = MISSING
 
     # whether to run the experiment only locally
     dry_run: bool = False
@@ -26,6 +25,7 @@ class ExperimentParams:
 class DatasetParams:
     # name of the dataset on huggingface
     name: str
+    # subconfig i.e. strict-small
     subconfig: str
 
 
@@ -62,16 +62,25 @@ class ModelParams:
     resume_checkpoint_path: Optional[str] = None
 
 
-
 @dataclass
-class ObjectiveParams:
-    # training objective parameters
+class TrainerParams:
+    batch_size: int
+    lr: float
+    num_warmup_steps: int
+    max_training_steps: int
+
+
+### Curriculum learning parameter: can be either objective or data-driven ###
+
+
+## bjective curriculum learning parameters ##
+@dataclass
+class ObjectiveCurriculumUnitParams:
+    # any curriculum requires the following parameters
     name: str
-    # NOTE: the objective can have arbitrary parameters, so
-    # we duck-type everything to be optional
-    # Custom MLM-Related parameters
-    # See: https://github.com/phueb/BabyBERTa/blob/master/babyberta/dataset.py#L250
-    # For information on how custom MLM is implemented
+    mask_probability: float
+
+    # Additional optional kwargs dependent on the objective curriculum unit
     num_mask_patterns: Optional[int] = None
     mask_pattern_size: Optional[int] = None
     probabilistic_masking: Optional[bool] = None
@@ -79,26 +88,39 @@ class ObjectiveParams:
     leave_unmasked_prob: Optional[float] = None
     random_token_prob: Optional[float] = None
     consecutive_masking: Optional[bool] = None
-    # mask_probability is used by every mlm objective (i.e. custom and base)
-    mask_probability: Optional[float] = None
     lexical_class: Optional[List[str]] = None
 
-@dataclass
-class CurriculumParams:
-    units: Any
-    steps: Dict[int, str]
 
 @dataclass
-class TrainerParams:
-    batch_size: int
-    lr: float
-    num_warmup_steps: int
-    max_training_steps: int
+class ObjectiveCurriculumParams:
+    # objective curriculum learning parameters
+
+    units: Dict[str, ObjectiveCurriculumUnitParams]
+    steps: Dict[int, str]
+
+
+## Data-driven curriculum learning parameters ##
+@dataclass
+class PacingFunctionParams:
+    end_percent: float
+    start_percent: float
+    num_steps: int
+
+
+@dataclass
+class DataCurriculumParams:
+    # data-driven curriculum learning parameters
+
     # the column of the data to sort by (aka n_gram perplexity, sentence length, etc.)
-    scoring_fn: Optional[str] = None
+    scoring_fn: str
+
     # one of ['linear', 'quad', 'root', 'step', 'exp', 'log'] or None, meaning no pacing
-    pacing_fn: Optional[str] = None
-    pacing_fn_kwargs: Optional[Dict[str, Union[int, float]]] = None
+    pacing_fn: str
+
+    pacing_fn_kwargs: PacingFunctionParams
+
+
+### Container for entire config ###
 
 
 @dataclass
@@ -108,5 +130,6 @@ class BabyLMConfig:
     tokenizer: TokenizerParams
     data_preprocessing: DataPreprocessingParams
     model: ModelParams
-    curriculum: CurriculumParams
     trainer: TrainerParams
+    objective_curriculum: ObjectiveCurriculumParams
+    data_curriculum: Optional[DataCurriculumParams] = None
