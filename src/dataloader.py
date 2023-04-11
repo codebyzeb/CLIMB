a6ed24843e2457ef1ba17cfa2064ca09e3fbc018
rdiehlmartinez <richarddiehl@gmail.com>
2023-04-11 13:48:54 +0100

    updating type annotations and type-related checks to dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -3,89 +3,22 @@
 import logging
 
 # typing imports
-from typing import List, Optional
+from typing import Dict, List, Optional
 
+from torch import Tensor
 from torch.utils.data import DataLoader
 from torch.utils.data._utils.pin_memory import pin_memory as _torch_pin_memory
 from torch.utils.data.dataloader import _BaseDataLoaderIter, _DatasetKind
 from torch.utils.data.datapipes.datapipe import IterDataPipe, MapDataPipe
-from transformers import PreTrainedTokenizer
+from transformers import PreTrainedTokenizerBase
 
 from src.objective import load_objective_collator
 
 from .config import ObjectiveCurriculumParams
+from .datasampler import CurriculumSampler, DistributedCurriculumSampler
 
 logger = logging.getLogger(__name__)
 objective_cl_logger = logging.getLogger("Objective Curriculum")
-
-
-class _CustomSingleProcessDataLoaderIter(_BaseDataLoaderIter):
-    def __init__(self, loader):
-        super().__init__(loader)
-        assert self._timeout == 0
-        assert self._num_workers == 0
-
-        self.loader = loader
-        self._sampler_iter = iter(self._index_sampler)
-
-        if isinstance(self._dataset, (IterDataPipe, MapDataPipe)):
-            raise NotImplementedError(
-                "IterDataPipe and MapDataPipe are not supported yet"
-            )
-
-        self._dataset_fetcher = _DatasetKind.create_fetcher(
-            self._dataset_kind,
-            self._dataset,
-            self._auto_collation,
-            self._collate_fn,
-            self._drop_last,
-        )
-
-    def _next_index(self):
-        idx = next(self._sampler_iter)
-        return idx
-
-    def _next_data(self):
-        """
-        Returns next data from this iterator.
-        """
-        assert (
-            self.loader.sampler.global_stepnum == self.loader.global_stepnum
-        ), "The global stepnum of the sampler and the dataloader are not the same"
-
-        index = self._next_index()  # may raise StopIteration
-
-        # based on the global_stepnum, we might create a new dataset fetcher with a different collate fn
-        if (
-            self.loader.global_stepnum
-            in self.loader.objective_curriculum.steps.keys()
-        ):
-            objective_cl_logger.info(
-                f"Setting curriculum at step: {self.loader.global_stepnum}"
-            )
-
-            self._collate_fn = load_objective_collator(
-                curriculum=self.loader.objective_curriculum,
-                tokenizer=self.loader.tokenizer,
-                step=self.loader.global_stepnum,
-            )
-            self._dataset_fetcher = _DatasetKind.create_fetcher(
-                self._dataset_kind,
-                self._dataset,
-                self._auto_collation,
-                self._collate_fn,
-                self._drop_last,
-            )
-
-        data = self._dataset_fetcher.fetch(index)  # may raise StopIteration
-        if self._pin_memory:
-            data = _torch_pin_memory(data, self._pin_memory_device)
-
-        # remove ignored columns
-        for ignore_column in self.loader.ignore_columns:
-            data.pop(ignore_column, None)
-
-        return data
 
 
 class CurriculumDataLoader(DataLoader):
@@ -93,7 +26,7 @@ class CurriculumDataLoader(DataLoader):
         self,
         global_stepnum: int,
         objective_curriculum: ObjectiveCurriculumParams,
-        tokenizer: PreTrainedTokenizer,
+        tokenizer: PreTrainedTokenizerBase,
         ignore_columns: Optional[List[str]] = None,
         num_workers: int = 0,
         **kwargs,
@@ -130,3 +63,81 @@ class CurriculumDataLoader(DataLoader):
 
     def __iter__(self):
         return _CustomSingleProcessDataLoaderIter(self)
+
+
+class _CustomSingleProcessDataLoaderIter(_BaseDataLoaderIter):
+    def __init__(self, loader: CurriculumDataLoader):
+        super().__init__(loader)
+        assert self._timeout == 0
+        assert self._num_workers == 0
+
+        self.loader = loader
+
+        if isinstance(self._dataset, (IterDataPipe, MapDataPipe)):
+            raise NotImplementedError(
+                "IterDataPipe and MapDataPipe are not supported yet"
+            )
+
+        self._dataset_fetcher = _DatasetKind.create_fetcher(
+            self._dataset_kind,
+            self._dataset,
+            self._auto_collation,
+            self._collate_fn,
+            self._drop_last,
+        )
+
+    def _next_index(self):
+        idx = next(self._sampler_iter)
+        return idx
+
+    def _next_data(self):
+        """
+        Returns next data from this iterator.
+        """
+
+        if not isinstance(
+            self.loader.sampler,
+            (CurriculumSampler, DistributedCurriculumSampler),
+        ):
+            return super()._next_data()
+
+        assert (
+            self.loader.sampler.global_stepnum == self.loader.global_stepnum
+        ), "The global stepnum of the sampler and the dataloader are not the same"
+
+        index = self._next_index()  # may raise StopIteration
+
+        # based on the global_stepnum, we might create a new dataset fetcher with a different collate fn
+        if (
+            self.loader.global_stepnum
+            in self.loader.objective_curriculum.steps.keys()
+        ):
+            logger.info(
+                f"(Curriculum Learning) Setting curriculum at step: {self.loader.global_stepnum}"
+            )
+
+            self._collate_fn = load_objective_collator(
+                curriculum=self.loader.objective_curriculum,
+                tokenizer=self.loader.tokenizer,
+                step=self.loader.global_stepnum,
+            )
+            self._dataset_fetcher = _DatasetKind.create_fetcher(
+                self._dataset_kind,
+                self._dataset,
+                self._auto_collation,
+                self._collate_fn,
+                self._drop_last,
+            )
+
+        data: Dict[str, Tensor] = self._dataset_fetcher.fetch(
+            index
+        )  # may raise StopIteration
+        if self._pin_memory:
+            data = _torch_pin_memory(data, self._pin_memory_device)  # type: ignore[arg-type]
+
+        # remove ignored columns
+        if self.loader.ignore_columns is not None:
+            for ignore_column in self.loader.ignore_columns:
+                data.pop(ignore_column, None)
+
+        return data
