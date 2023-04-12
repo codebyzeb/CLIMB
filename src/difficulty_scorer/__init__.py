d2679f609305e5be62a26a2aef5d2892e62b11b6
rdiehlmartinez <richarddiehl@gmail.com>
2023-04-12 16:52:54 +0100

    fleshing out skeleton structure of difficult_scorer package and ngram perplexity

--- src/difficulty_scorer/__init__.py
+++ src/difficulty_scorer/__init__.py
@@ -1,24 +1,29 @@
+from ..config import DifficultyScorerKwargsType
 from .base_difficulty_scorer import BaseDifficultyScorer
 from .ngram_perplexity import NGramPerplexityScorer
+from .registry import DIFFICULTY_SCORER_REGISTRY
 
 
 def get_difficulty_scorer(
     difficulty_scorer_name: str,
-    **kwargs,
+    difficulty_scorer_kwargs: DifficultyScorerKwargsType,
 ) -> BaseDifficultyScorer:
     """
     Returns a difficulty scorer based on the name.
 
     Args:
         * difficulty_scorer_name (str): The name of the difficulty scorer
-        * **kwargs: Additional keyword arguments
+        * difficulty_scorer_kwargs (DifficultyScorerKwargsType): The kwargs for the difficulty
+            scorer
     Returns:
         * BaseDifficultyScorer: A difficulty scorer
     """
 
-    if difficulty_scorer_name == "ngram_perplexity":
-        return NGramPerplexityScorer(**kwargs)
-
-    raise ValueError(
-        f"Difficulty Scorer {difficulty_scorer_name} not supported."
-    )
+    if difficulty_scorer_name in DIFFICULTY_SCORER_REGISTRY:
+        return DIFFICULTY_SCORER_REGISTRY[difficulty_scorer_name](
+            **difficulty_scorer_kwargs
+        )
+    else:
+        raise ValueError(
+            f"Difficulty Scorer {difficulty_scorer_name} not supported."
+        )
--- src/difficulty_scorer/base_difficulty_scorer.py
+++ src/difficulty_scorer/base_difficulty_scorer.py
@@ -1,27 +1,35 @@
 """ Implements an abstract base class for difficulty scorers. """
 
 from abc import ABCMeta, abstractmethod
-from typing import List
+from typing import Sequence
 
-from datasets import Dataset
-
-# typing imports
+from torch.utils.data import Dataset
 
 
 class BaseDifficultyScorer(metaclass=ABCMeta):
-    def __init__(self, **kwargs):
-        """Initializes the difficulty scorer."""
-        self._kwargs = kwargs
-
     @abstractmethod
-    def score_difficulty(self, dataset: Dataset, **kwargs) -> List[float]:
+    def score_difficulty(
+        self,
+        dataset: Dataset,
+        indices: Sequence[int],
+        global_stepnum: int,
+        max_difficulty_percentile: float,
+    ) -> Sequence[float]:
         """
-        Scores the difficulty of the dataset, and returns a list of scores.
+        Scores the difficulty of the dataset, and returns a sequence of scores.
 
         Args:
             * dataset (Dataset): The dataset to score
-            * **kwargs: Additional keyword arguments
+            * indices (Sequence[int]): The indices of the dataset to score
+                (in the same order as the dataset). This is used for distributed training, where
+                the dataset is split across multiple processes, and each process only has a subset
+                of the dataset.
+            * global_stepnum (int): The global step number of the training loop
+            * max_difficulty_percentile (float): The maximum difficulty percentile to use
         Returns:
-            * List[float]: A list of scores, one for each example in the dataset
+            * difficulty_scores: A list of difficulty scores that correspond to the difficulty of
+                each sample in the passed in dataset (in the same order as the dataset).
+                The difficulty scores that are above the max_difficulty_percentile should be set
+                to 0.
         """
         raise NotImplementedError
