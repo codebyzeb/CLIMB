da5707332721f261102b5fc8da1ca5c2ca8d76a2
rdiehlmartinez <richarddiehl@gmail.com>
2023-04-12 16:52:54 +0100

    fleshing out skeleton structure of difficult_scorer package and ngram perplexity

--- src/difficulty_scorer/ngram_perplexity.py
+++ src/difficulty_scorer/ngram_perplexity.py
@@ -4,23 +4,27 @@ of a dataset based on the perplexity of a n-gram model trained on the dataset.
 """
 
 import logging
-from typing import List
+from typing import List, Sequence
+
+import numpy as np
 
 # typing imports
 from datasets import Dataset
-from nltk.lm import MLE
-from nltk.lm.preprocessing import padded_everygram_pipeline
-
-from .base_difficulty_scorer import BaseDifficultyScorer
 
 # NLTK Package provides functionality for n-gram models
+from nltk.lm import MLE
+from nltk.util import everygrams
+from transformers import PreTrainedTokenizerFast
 
+from .base_difficulty_scorer import BaseDifficultyScorer
+from .registry import register_difficulty_scorer
 
 logger = logging.getLogger("DifficultyScorer")
 
 
+@register_difficulty_scorer("ngram_perplexity")
 class NGramPerplexityScorer(BaseDifficultyScorer):
-    def __init__(self, n_gram=2, **kwargs):
+    def __init__(self, n_gram: int, tokenizer: PreTrainedTokenizerFast):
         """
         Initializes the n-gram perplexity scorer.
 
@@ -28,14 +32,15 @@ class NGramPerplexityScorer(BaseDifficultyScorer):
             * n_gram (int): The n-gram to use for the n-gram model
         """
 
-        self.n_gram = n_gram
-
         logger.info("Initializing n-gram perplexity scorer")
 
-        super().__init__(**kwargs)
+        self.n_gram = n_gram
+
+        self.tokenizer = tokenizer
 
     def _train_model(self, dataset: Dataset):
-        """Trains a n-gram model on the dataset.
+        """
+        Trains a n-gram model on the dataset.
 
         Args:
             * dataset (Dataset): The dataset to train the model on
@@ -43,18 +48,85 @@ class NGramPerplexityScorer(BaseDifficultyScorer):
             * None
         """
 
-        train, vocab = padded_everygram_pipeline(self.n_gram, dataset)
-        lm = MLE(2)
-        lm.fit(train, vocab)
+        self.tokenized_dataset = dataset.map(
+            lambda x: {
+                "tokenized_text": self.tokenizer.tokenize(
+                    x["text"], add_special_tokens=True
+                )
+            },
+            remove_columns=["text"],
+            num_proc=64,
+        )
 
-    def score_difficulty(self, dataset: Dataset, **kwargs) -> List[float]:
-        """Scores the difficulty of the dataset, and returns a list of scores.
+        # We split the tokenized dataset into n-gram that we can feed into the n-gram model
+        train_data_n_grams = (
+            everygrams(sent, max_len=self.n_gram)
+            for sent in self.tokenized_dataset
+        )
+        train_vocab = list(self.tokenizer.vocab.keys())
+
+        self.lm = MLE(self.n_gram)
+        self.lm.fit(train_data_n_grams, train_vocab)
+
+    def score_difficulty(
+        self,
+        dataset: Dataset,
+        indices: List[int],
+        global_stepnum: int,
+        max_difficulty_percentile: float,
+    ) -> Sequence[float]:
+        """
+        Scores the difficulty of the dataset, and returns a list of scores.
 
         Args:
             * dataset (Dataset): The dataset to score
-            * **kwargs: Additional keyword arguments
+            * indices (Sequence[int]): The indices of the dataset to score
+                (in the same order as the dataset). This is used for distributed training, where
+                the dataset is split across multiple processes, and each process only has a subset
+                of the dataset.
+            * global_stepnum (int): The global step number of the training loop
+            * max_difficulty_percentile (float): The maximum difficulty percentile to use
         Returns:
-            * List[float]: A list of scores, one for each example in the dataset
+            * difficulty_scores: A list of difficulty scores that correspond to the difficulty of
+                each sample in the passed in dataset (in the same order as the dataset).
+                The difficulty scores that are above the max_difficulty_percentile should be set
+                to 0.
         """
 
-        return [0.0]
+        if global_stepnum == 0:
+            self._train_model(dataset)
+
+            assert hasattr(self, "lm"), "n-gram model not trained"
+
+            difficulty_scores: Sequence[float] = []
+
+            data_n_grams = (
+                everygrams(sent, max_len=self.n_gram)
+                for sent in self.tokenized_dataset
+            )
+
+            next_idx = indices[0]
+
+            for _idx, n_gram in enumerate(data_n_grams):
+                if _idx == next_idx:
+                    difficulty_scores.append(self.lm.perplexity(n_gram))
+                    indices.pop(0)
+                    if len(indices) == 0:
+                        break
+                    next_idx = indices[0]
+
+            # convert difficulty scores to percentiles
+            max_difficulty = float(
+                np.percentile(
+                    difficulty_scores, max_difficulty_percentile * 100
+                )
+            )
+
+            # Set difficulty scores that are above the max difficulty percentile to 0
+            difficulty_scores = [
+                score if score <= max_difficulty else 0.0
+                for score in difficulty_scores
+            ]
+            self._difficulty_scores = difficulty_scores
+
+        return self._difficulty_scores
---
+++ src/difficulty_scorer/registry.py
@@ -0,0 +1,15 @@
+from typing import Mapping, Type, TypeVar
+
+from base_difficulty_scorer import BaseDifficultyScorer
+
+T = TypeVar("T", bound=BaseDifficultyScorer)
+
+DIFFICULTY_SCORER_REGISTRY: Mapping[str, Type[BaseDifficultyScorer]] = {}
+
+
+def register_difficulty_scorer(name: str):
+    def _register(cls: Type[T]) -> Type[T]:
+        DIFFICULTY_SCORER_REGISTRY[name] = cls
+        return cls
+
+    return _register
