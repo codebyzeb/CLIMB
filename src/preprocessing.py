9ab5e60a798a9bdeb450b3e0743205ced0d2dce7
Zebulon Goriely <zepsilon32@gmail.com>
2023-04-15 16:15:11 +0100

    Implement concatenation of input

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,7 +8,6 @@ from transformers import PreTrainedTokenizerFast
 
 from .config import BabyLMConfig
 
-
 class DataPreprocessor(object):
     def __init__(self, cfg: BabyLMConfig, tokenizer: PreTrainedTokenizerFast):
         """
@@ -20,6 +19,7 @@ class DataPreprocessor(object):
         # data processing params
         self.include_punctuation = cfg.data_preprocessing.include_punctuation
         self.max_input_length = cfg.data_preprocessing.max_input_length
+        self.concat_input = cfg.data_preprocessing.concat_input
         self.callback_functions = cfg.data_preprocessing.callback_functions
 
         self.tokenizer = tokenizer
@@ -40,20 +40,37 @@ class DataPreprocessor(object):
                 for line in examples["text"]
             ]
 
+        # concatenate the input text if concat_input is True then split into chunks of max_input_length
+        if self.concat_input:
+            joined_text = " ".join(examples["text"])
+            tokenized = self.tokenizer(
+                joined_text,
+                padding="max_length",
+                max_length=self.max_input_length,
+                truncation=False,
+                return_special_tokens_mask=True
+            )
+
+            batch = {'input_ids' : [], 'special_tokens_mask' : [], 'attention_mask' : []}
+            examples['text'] = []
+            for i in range(0, len(tokenized['input_ids']) - self.max_input_length+1, self.max_input_length):
+                batch['input_ids'].append(tokenized['input_ids'][i:i+self.max_input_length])
+                batch['special_tokens_mask'].append(tokenized['special_tokens_mask'][i:i+self.max_input_length])
+                batch['attention_mask'].append(tokenized['attention_mask'][i:i+self.max_input_length])
+                examples['text'].append(joined_text[i:i+self.max_input_length])
+        else:
+            batch = self.tokenizer(
+                examples['text'],
+                padding="max_length",
+                truncation=True,
+                max_length=self.max_input_length,
+                return_special_tokens_mask=True
+            )
+
         if self.callback_functions:
             for callback_function in self.callback_functions:
                 examples[callback_function] = getattr(self, callback_function)(
-                    examples["text"]
+                    examples['text']
                 )
 
-        # tokenize the input text
-
-        tokenized_output = self.tokenizer(
-            examples["text"],
-            padding="max_length",
-            truncation=True,
-            max_length=self.max_input_length,
-            return_special_tokens_mask=True,
-        )
-
-        return tokenized_output
+        return batch
