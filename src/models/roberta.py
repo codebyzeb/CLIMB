dfb97421108126b52fac07b5a4e16eaf21280af6
rdiehlmartinez <richarddiehl@gmail.com>
2023-04-25 18:29:28 +0100

    when loading in models we now only load in the base model (i.e. without the task head)

--- src/models/roberta.py
+++ src/models/roberta.py
@@ -6,49 +6,19 @@ Model classes use the Roberta Architecture
 
 from transformers import (
     RobertaConfig,
-    RobertaForCausalLM,
-    RobertaForMaskedLM,
+    RobertaModel,
     RobertaPreLayerNormConfig,
-    RobertaPreLayerNormForCausalLM,
-    RobertaPreLayerNormForMaskedLM,
+    RobertaPreLayerNormModel,
 )
 
 ### Wrapping the Roberta models to make them compatible with the model registry ###
 
 
-@register_model("roberta_for_causal_lm", RobertaConfig)
-class DefaultRobertaForCausalLM(RobertaForCausalLM):
+@register_model("roberta_pre_layer_norm", RobertaPreLayerNormConfig)
+class BaseRobertaPreLayerNormModel(RobertaPreLayerNormModel):
     pass
 
 
-@register_model("roberta_for_masked_lm", RobertaConfig)
-class DefaultRobertaForMaskedLM(RobertaForMaskedLM):
+@register_model("roberta", RobertaConfig)
+class BaseRobertModel(RobertaModel):
     pass
-
-
-@register_model(
-    "roberta_pre_layer_norm_for_causal_lm", RobertaPreLayerNormConfig
-)
-class DefaultRobertaPreLayerNormForCausalLM(RobertaPreLayerNormForCausalLM):
-    pass
-
-
-@register_model(
-    "roberta_pre_layer_norm_for_masked_lm", RobertaPreLayerNormConfig
-)
-class DefaultRobertaPreLayerNormForMaskedLM(RobertaPreLayerNormForMaskedLM):
-    pass
-
-
-### Custom Roberta Models ###
-
-# NOTE: The forward pass of these models always needs to return ModelOutput
-#       objects. See the documentation for more details.
-
-
-@register_model(
-    "tuned_roberta_pre_layer_norm_for_masked_lm", RobertaPreLayerNormConfig
-)
-class TunedRobertaPreLayerNormForMaskedLM(RobertaPreLayerNormForMaskedLM):
-    pass
-    # TODO: Insert any code to overwrite the standard behavior
