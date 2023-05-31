d6b988b641f8ff88a71d4ae74f0069b39db71c1f
Zeb <zepsilon32@gmail.com>
2023-05-31 10:53:33 +0100

    Add POS vocabulary map

--- src/vocabulary_curriculum/vocabulary_map/base_map.py
+++ src/vocabulary_curriculum/vocabulary_map/base_map.py
@@ -1,7 +1,7 @@
 """ Implements an abstract base class for restricting the vocabulary during training. """
 
 from abc import ABCMeta, abstractmethod
-from typing import Callable
+from typing import Callable, Dict
 
 from torch import Tensor
 from transformers import PreTrainedTokenizerFast
@@ -16,22 +16,29 @@ class BaseVocabularyMap(metaclass=ABCMeta):
         """
         Args:
             * tokenizer (PreTrainedTokenizer): The tokenizer used for preprocessing the data
-            * pacing_fn (Callable[[int], float]): A function that takes in the global step number
+            * pacing_fn (Callable[[int], float]): The pacing function, returns a percentage when given the global step number
         """
         self.tokenizer = tokenizer
         self.pacing_fn = pacing_fn
+        self.unk_token_id = (
+            tokenizer.unk_token_id
+            if tokenizer.unk_token_id is not None
+            else tokenizer.all_special_ids[-1]
+        )
 
     @abstractmethod
     def map_tokens(
         self,
-        ids: Tensor,
+        data: Dict[str, Tensor],
+        key: str,
         global_stepnum: int,
     ) -> Tensor:
         """
         Map a tensor of token ids to a tensor of token ids with difficult tokens mapped to <unk>.
 
         Args:
-            * ids (Tensor): A Tensor containing token ids
+            * data (Dict[str, Tensor]): A dictionary containing the data
+            * key (str): The key of the data to map
             * global_stepnum (int): The global step number of the training loop
         Returns:
             * mapped_ids (Tensor): A Tensor containing the token ids with difficult tokens mapped to <unk>
