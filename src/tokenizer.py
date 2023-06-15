09fe431ad7810abb816c7e881f93c9ccbe90a0b4
rdiehlmartinez <richarddiehl@gmail.com>
2023-06-15 15:57:54 +0100

    simplifying the loading in of tokenizer - no longer enabling re-training, only loads in from pretrained

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -4,7 +4,6 @@ import logging
 import os
 
 # typing imports
-from datasets import DatasetDict
 from transformers import AutoTokenizer, PreTrainedTokenizerFast
 
 from src.config import BabyLMConfig
@@ -12,23 +11,16 @@ from src.config import BabyLMConfig
 logger = logging.getLogger(__name__)
 
 
-def load_tokenizer(
-    cfg: BabyLMConfig, dataset: DatasetDict
-) -> PreTrainedTokenizerFast:
+def load_tokenizer(cfg: BabyLMConfig) -> PreTrainedTokenizerFast:
     """
     Sets up tokenizer for the model, based on tokenizer configurations
 
     Args:
         cfg (BabyLMConfig): hydra config object
-        dataset (Dataset): instantiated dataset object
     """
 
-    full_tokenizer_name = cfg.tokenizer.name
-    org_name = full_tokenizer_name.split("/")[0]
-    tokenizer_name = full_tokenizer_name.split("/")[1]
-
-    # anything that's not name and vocab_size is an optional tokenizer kwarg
-    remove_keys = ["name", "vocab_size"]
+    # anything that's not 'name' is an optional argument to the tokenizer
+    remove_keys = ["name"]
     tokenizer_kwargs = {
         str(key): val
         for key, val in cfg.tokenizer.items()
@@ -36,7 +28,7 @@ def load_tokenizer(
     }
 
     tokenizer: PreTrainedTokenizerFast = AutoTokenizer.from_pretrained(
-        full_tokenizer_name,
+        cfg.tokenizer.name,
         use_auth_token=os.environ["HF_READ_TOKEN"],
         **tokenizer_kwargs,
     )  # type: ignore
@@ -44,19 +36,5 @@ def load_tokenizer(
     assert isinstance(
         tokenizer, PreTrainedTokenizerFast
     ), "Tokenizer needs to be a PreTrainedTokenizerFast"
-
-    if org_name != "CamBabyTrainers":
-        logger.info("Loading in tokenizer from third-party org")
-        logger.info("Retraining tokenizer on training dataset")
-
-        tokenizer = tokenizer.train_new_from_iterator(
-            dataset["train"]["text"], vocab_size=cfg.tokenizer.vocab_size
-        )
-
-        new_tokenizer_name = f"CamBabyTrainers/{tokenizer_name}-{cfg.tokenizer.vocab_size}-tokenizer"
-        logger.info(f"Pushing trained tokenizer to hub: {new_tokenizer_name}")
-        tokenizer.push_to_hub(
-            new_tokenizer_name, use_auth_token=os.environ["HF_WRITE_TOKEN"]
-        )
 
     return tokenizer
