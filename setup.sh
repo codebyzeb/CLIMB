a175de7217cb65415269ea02544189c48c74905d
Richard Diehl Martinez <richarddiehl@gmail.com>
2023-10-27 15:11:09 +0100

    cleaning up old documentation and setup scripts from babylm

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
-poetry shell 
-git lfs install
-git submodule update --init
-poetry install --sync
-cd lib/evaluation-pipeline
-unzip filter_data.zip
-pre-commit install
-huggingface-cli login
-wandb login
+read -p "Is this your first time setup? (yes/no) " response
+if [ "$response" = "yes" ]; then
+    git lfs install
+    git submodule update --init
+    poetry install --sync
+    poetry shell 
+    cd lib/evaluation-pipeline
+    unzip filter_data.zip
+    pre-commit install
+    huggingface-cli login
+    wandb login 
+fi
+
 source .env
